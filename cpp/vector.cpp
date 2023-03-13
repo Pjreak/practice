b2394a55251f4d25beefd71491a4d8648cb5ba90
abel <leipu700@gmail.com>
2023-03-13 20:55:51 +0800

    change vecor.cpp

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -1,1 +1,27 @@
- 
+#include <iostream>
+
+int main(){
+    using namespace std;
+
+    char*name=new char[20];
+    int age;
+
+    cout<<"what's your name ?"<<endl;
+
+    cin>>name;
+
+    cout<<"what's your age?"<<endl;
+
+    cin>>age;
+
+    cout<<"Hello "<<name<<"  I know you have "<<age<<endl;
+
+    delete [] name;
+
+
+
+
+
+
+}
+
