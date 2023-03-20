f014002077e89890dfd5073de218c570a5f9caaa
abel <leipu700@gmail.com>
2023-03-20 18:24:06 +0800

    cpp editer

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -7,12 +7,22 @@ using namespace std;
 
 void func()
 {
-    throw std::invalid_argument("hehheheh");
+    std::vector<int> vector_1(4);
+
+    for(int e:vector_1){
+        std::cout<<e<<std::endl;
+    }
+    int r=2322;
+    int t=33;
+    int& i=r;
+    int * pInt=&r;
+    i=t;
+    std::cout<<i<<std::endl;
+    std::cout<<&i<<endl<<&r<<std::endl<<&t<<std::endl<<&pInt<<std::endl;
+    std::cout<<r<<std::endl;
 }
 
 int main(int argc, char **argv)
 {
-    cout << "heh" << endl;
     func();
-    cout << 1223 << endl;
 }
